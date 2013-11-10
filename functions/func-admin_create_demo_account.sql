05db96454044e8d25100070203278f3211438375
David A. Brown <dabrown7@gmail.com>
2013-11-10 10:45:02 -0800

    admin_create_demo_account: unify retvals
    - Updated retvals
    - Updated eventcodes
    - Replaced magic with constants
    - Returns new add_member/add_acccount result directly

--- functions/func-admin_create_demo_account.sql
+++ functions/func-admin_create_demo_account.sql
@@ -4,16 +4,31 @@
 -- -----------------------------------------------------------------------------
 -- Creates a DEMONSTRATION account with two members.  It expires after 1 WEEK.
 -- This function is run automatically by the database installation script.
--- Returns 1 on success, 0 if this account already exists.
+--    > 1: MID of newly created Member
+--    -20: Member (Account owner) exists with this e-mail address
+--    -22: Member already exists with this e-mail address
+--    -23: Member already exists with this UserID
+--    -24: Member already exists with this Name in the Account
 -- -----------------------------------------------------------------------------
 -- 2013-10-02 dbrown Created
 -- 2013-10-16 dbrown Fixed outdated call to add_member()
 -- 2013-10-24 dbrown Fixed outdated call to add_member() [h_profilepic]
+-- 2013-11-09 dbrown Updated retvals, eventcodes, replaced magic with constants
+--                   Returns new add_member or add_account result directly
 -----------------------------------------------------------------------------
 
 create or replace function admin_create_demo_account() returns int as $$
 
 declare
+    EC_USERERR_ADDING_ACCOUNT constant varchar := '4020';
+    EC_OK_ADDED_MEMBER              constant varchar := '1030';
+    EC_USERERR_ADDING_MEMBER        constant varchar := '4030';
+    EC_DEVERR_ADDING_MEMBER         constant varchar := '9030';
+    RETVAL_ERR_ACCOUNT_EXISTS       constant int := -20;
+    RETVAL_ERR_MEMBER_EXISTS_EMAIL  constant int := -22;
+    RETVAL_ERR_MEMBER_EXISTS_USERID constant int := -23;
+    RETVAL_ERR_MEMBER_EXISTS_NAME   constant int := -24;
+
     cid int;
     mid int;
     
@@ -23,44 +38,24 @@ declare
     A_MI         varchar := 'A';
     A_LNAME      varchar := 'Owner';
     A_EXP      timestamp := current_date + interval '1 week';
-    A_REFERRER   varchar := '';
-    A_ADDRESS1   varchar := '800 Building Ave';
-    A_ADDRESS2   varchar := 'Suite 165';
-    A_CITY       varchar := 'New York';
-    A_STATE      varchar := 'NY';
-    A_POSTALCODE varchar := '10010';
-    A_COUNTRY    varchar := 'US';
-    A_PHONE      varchar := '800-235-8332';
     
     M_USERID     varchar := 'demo';
     M_EMAIL      varchar := 'user@lifecloud.info';
     M_PWORD      varchar := 'demo';
     M_FNAME      varchar := 'Demo';
-    M_MI         varchar := 'M';
     M_LNAME      varchar := 'User';
-    M_ADDRESS1   varchar := '12345 Some Street';
-    M_ADDRESS2   varchar := 'Apt 444';
-    M_CITY       varchar := 'Chicago';
-    M_STATE      varchar := 'IL';
-    M_POSTALCODE varchar := '55555';
-    M_COUNTRY    varchar := 'US';
-    M_PHONE      varchar := '123-456-7890';
     
 begin
 
-    cid := add_account( 
-        A_EMAIL, A_PWORD, A_LNAME, A_FNAME, A_MI, A_EXP, A_REFERRER, 
-        A_ADDRESS1, A_ADDRESS2, A_CITY, A_STATE, A_POSTALCODE, A_COUNTRY, A_PHONE );
+    -- Add an account.  This automagically creates an Owner member.
+    cid := add_account( A_EMAIL, A_PWORD, A_LNAME, A_FNAME, A_MI, A_EXP );
     
-    if (cid < 1) then return 0; end if; -- Demo account already exists
-    
-    -- Add an extra (non-admin) member
-    
-    mid := add_member( cid,
-        M_FNAME, M_LNAME, M_MI, M_PWORD, M_USERID, M_EMAIL, null,
-        M_ADDRESS1, M_ADDRESS2, M_CITY, M_STATE, M_POSTALCODE, M_COUNTRY, M_PHONE );
+    if (cid < 1) then return cid; end if;
         
-    return 1;
+    -- Add an extra (non-admin) member.  This account now has TWO members.
+    mid := add_member( cid, M_FNAME, M_LNAME, M_MI, M_PWORD, M_USERID, M_EMAIL, null );
+        
+    return mid;
     
 end;
 $$ language plpgsql;
