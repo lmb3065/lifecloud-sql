b9583df072e17d262b46cfe0dab64ab375d9f5c7
David A. Brown <dabrown7@gmail.com>
2013-11-11 14:43:22 -0800

    delete_* : unify retvals
    - update retvals and eventcodes
    - replace magic with constants
    - remove unnecessary sanity checks
    - include name of deleted item in eventlog

--- functions/func-delete_file.sql
+++ functions/func-delete_file.sql
@@ -23,60 +23,59 @@ declare
     EC_OK_DELETED_FILE          constant varchar := '1087';
     EC_OK_OWNER_DELETED_FILE    constant varchar := '1088';
     EC_OK_ADMIN_DELETED_FILE    constant varchar := '1089';
-    EC_DEVERR_DELETING_FILE     constant varchar := '9089';
-    RETVAL_SUCCESS              constant int :=   1;
-    RETVAL_ERR_FILE_NOTFOUND    constant int := -14;
-    
-    result int;
-    source_cid int;
-    source_ulevel int;
-    source_isadmin int;
-    target_mid int;
-    target_cid int;
-    filename text;
+    EC_PERMERR_DELETING_FILE    constant varchar := '6087';
+    EC_DEVERR_DELETING_FILE     constant varchar := '9087';
     eventcode_out varchar;
-    nrows int;
+
+    RETVAL_SUCCESS          constant int :=   1;
+    RETVAL_FILE_NOTFOUND    constant int := -14;
+    result int;
+    
+    source_cid      int;
+    source_ulevel   int; 
+    source_isadmin  int;
+    target_mid      int;
+    target_cid      int;
+    file_name       text;
 
 begin
 
     -- Get target file's owner
-    SELECT f.mid, fdecrypt(f.x_name) INTO target_mid, filename 
-        FROM files f
-        WHERE f.uid = file_uid;
+    SELECT mid, fdecrypt(x_name) INTO target_mid, file_name 
+        FROM files
+        WHERE uid = file_uid;
     
     if (target_mid is null) then
-        perform log_event( source_cid, source_mid, EC_DEVERR_DELETING_FILE, 'File does not exist' );
-        return RETVAL_ERR_FILE_NOTFOUND;
+        perform log_event( source_cid, source_mid, EC_DEVERR_DELETING_FILE,
+                    'File does not exist' );
+        return RETVAL_FILE_NOTFOUND;
     end if;
     
     
     -- Check relation between this user and the file's owner
-    select allowed, scid, slevel, sisadmin, tcid 
-        into result, source_cid, source_ulevel, source_isadmin, target_cid
-        from member_can_update_member(source_mid, target_mid);
+    SELECT allowed, scid, slevel, sisadmin, tcid 
+        INTO result, source_cid, source_ulevel, source_isadmin, target_cid
+        FROM member_can_update_member(source_mid, target_mid);
 
     if (result < 1) then 
-        perform log_permissions_error( DEVERR_DELETING_FILE, result,
+        perform log_permissions_error( EC_PERMERR_DELETING_FILE, result,
                     source_cid, source_mid, target_cid, target_mid );
         return result;
     end if;
     
     
     -- Delete the file
-    delete from files where files.uid = file_uid;
+    delete from Files where uid = file_uid;
     
     
     -- Success
-    if (source_isadmin > 0) then 
-        eventcode_out := EC_OK_ADMIN_DELETED_FILE;
-    elsif (source_ulevel = 0) and (source_mid <> target_mid) then 
-        eventcode_out := EC_OK_OWNER_DELETED_FILE;
-    else
-        eventcode_out := EC_OK_DELETED_FILE;
+    if (source_mid = target_mid) then eventcode_out := EC_OK_DELETED_FILE;
+    elsif (source_isadmin = 1)   then eventcode_out := EC_OK_ADMIN_DELETED_FILE;
+    elsif (source_ulevel <= 1)   then eventcode_out := EC_OK_OWNER_DELETED_FILE;
+    else                              eventcode_out := EC_OK_DELETED_FILE;
     end if;
-
     
-    perform log_event( source_cid, source_mid, eventcode_out, filename, target_cid, target_mid);
+    perform log_event( source_cid, source_mid, eventcode_out, file_name, target_cid, target_mid);
     return RETVAL_SUCCESS;
         
 end;
