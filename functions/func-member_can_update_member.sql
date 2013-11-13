b5ff153356c0c600e6696a5e85cfeb503fb75c60
David A. Brown <dabrown7@gmail.com>
2013-11-12 16:36:17 -0800

    member_can_update_member: Updated to newly verified specs
    - isAdmin 1   : ADMIN, can modify ANYONE
    - userlevel 0 : OWNER, can modify all Account members
    - userlevel 1 : TRUSTED, can modify all Account members except OWNER
    - userlevel 2 : USER, can modify self only
    - userlevel 3+ : VIEWER, can modify nothing

--- functions/func-member_can_update_member.sql
+++ functions/func-member_can_update_member.sql
@@ -11,22 +11,20 @@
 --   0: No, a required argument was NULL
 --  -11: No, source Member could not be found
 --  -12: No, target Member could not be found
---  -80: No, source Member userLevel > max_userlevel
---  -81: No, source and target Members are in different CIDs *
---  -82: No, target Member outranks source Member
+--  -88: No, source Member is not allowed to modify target Member 
 -- The rest of the columns are their permissions fields pulled from Members.
 -------------------------------------------------------------------------------------------------
 -- 2013-10-13 dbrown: Created
 -- 2013-10-24 dbrown: Userlever 2 can now change themselves
 -- 2013-11-06 dbrown: All new return values; replaced magic numbers with constants;
---       Removed special case "if target is account owner, source can be only self or admin" 
+--               Removed special case "if target is owner, source can be only self or admin"
+-- 2013-11-12 dbrown: Ensure test types and orders conform to documentation; New logic
 -------------------------------------------------------------------------------------------------
 
 create or replace function member_can_update_member(
 
     source_mid      integer,
-    target_mid      integer,
-    max_userlevel   integer default 1
+    target_mid      integer
 
 ) returns table (
 
@@ -45,101 +43,100 @@ declare
     RETVAL_ERR_ARG_MISSING     constant int := 0;
     RETVAL_ERR_MEMBER_NOTFOUND constant int := -11;
     RETVAL_ERR_TARGET_NOTFOUND constant int := -12;
-    RETVAL_ERR_USERLEVEL       constant int := -80;
-    RETVAL_ERR_DIFFACCOUNTS    constant int := -81;
-    RETVAL_ERR_OUTRANKED       constant int := -82;
+    RETVAL_ERR_NOT_ALLOWED     constant int := -88;
 
-    nil      constant int = null;
-    source_cid        int = null;
-    source_userlevel  int = null;
-    source_isadmin    int = null;
+    ULEVEL_OWNER    constant int := 0; -- Can modify all Account members
+    ULEVEL_TRUSTED  constant int := 1; -- Can modify all Account members except Owner
+    ULEVEL_USER     constant int := 2; -- Can modify only themselves
+    ULEVEL_VIEWER   constant int := 3; -- Can modify nothing
     
-    target_cid        int = null;
-    target_userlevel  int = null;
-    target_isadmin    int = null;
+    nil      constant int = null;
+    source_cid     int = null;
+    source_ulevel  int = null;
+    source_isadmin int = null;
+    
+    target_cid      int = null;
+    target_ulevel   int = null;
+    target_isadmin  int = null;
 
 begin
 
     if (source_mid is null) or (target_mid is null) then 
-        return query select RETVAL_ERR_ARG_MISSING, nil, nil, nil, nil, nil, nil;
-        return; end if;
-    
-        
-    ----==== Grab and test the Source member first ====----
-
-    select cid, userlevel, isadmin
-        into source_cid, source_userlevel, source_isadmin
-        from Members where mid = source_mid;    
-    
-    if (source_cid is null) then
-        return query select RETVAL_ERR_MEMBER_NOTFOUND, nil, nil, nil, nil, nil, nil;
-        return; end if; 
-
-    -- Changing themselves? Return YES if userlevel in ( 0,1,2 )
-    if (source_mid = target_mid) then
-        if source_userlevel <= 2 then
-            return query select RETVAL_SUCCESS,
-                    source_cid, source_userlevel, source_isadmin,
-                    source_cid, source_userlevel, source_isadmin;
-            return;
-        else
-            return query select RETVAL_ERR_USERLEVEL,
-                    source_cid, source_userlevel, source_isadmin,
-                    nil, nil, nil;
-        end if;
-    end if;        
-
-    -- If the caller forced a NULL for max_userlevel, we won't check it
-    if (max_userlevel is not null) and (source_userlevel > max_userlevel) then
-        -- Exit: Source member has Insufficient UserLevel
-        return query select RETVAL_ERR_USERLEVEL,
-                    source_cid, source_userlevel, source_isadmin,
-                    nil, nil, nil;
-        return; end if;
-
-
-    
-    ----==== Grab and test the Target member ====----
-    
-    select cid, userlevel, isadmin
-        into target_cid, target_userlevel, target_isadmin
-        from Members where mid = target_mid;    
-            
-    if (target_cid is null) then 
-        -- Exit: Target member could not be found
-        return query select RETVAL_ERR_TARGET_NOTFOUND,
-                source_cid, source_userlevel, source_isadmin, nil, nil, nil;
-        return; end if; 
-
-        
-        
-    ----==== Test the relation between the two ====----
-        
-    if (source_cid <> target_cid) and (source_isadmin = 0) then
-        -- Exit: only Admins can alter Members of different Accounts
-        return query select RETVAL_ERR_DIFFACCOUNTS,
-                source_cid, source_userlevel, source_isadmin,
-                target_cid, target_userlevel, target_isadmin;
-        return; end if;    
-
-    if ((source_isadmin < target_isadmin)
-     or (source_userlevel > target_userlevel)) then
-         -- Exit: Target outranks Source
-        return query select RETVAL_ERR_OUTRANKED,
-                source_cid, source_userlevel, source_isadmin,
-                target_cid, target_userlevel, target_isadmin;
+        return query
+            SELECT RETVAL_ERR_ARG_MISSING, nil, nil, nil, nil, nil, nil;
         return;
+    end if;
+    
+        
+    --=============================================================================
+    -- Grab the SOURCE MEMBER from the database
+    
+    SELECT cid, userlevel, isadmin
+        INTO source_cid, source_ulevel, source_isadmin
+        FROM Members WHERE mid = source_mid;
+
+    -- Check for: [User-Member] doesn't exist -> NO
+    if (source_cid is null) then
+        return query SELECT RETVAL_ERR_MEMBER_NOTFOUND, nil, nil, nil, nil, nil, nil;
+        return;
+    end if;
+
+    -- Changing self?
+    if (source_mid = target_mid) then
+        if (source_ulevel >= ULEVEL_VIEWER) then
+            -- Viewers (and beyond) are not allowed that
+            return query SELECT RETVAL_ERR_NOT_ALLOWED, nil, nil, nil, nil, nil, nil;
+            return;
+        else -- Everyone else is OK
+            return query SELECT RETVAL_SUCCESS,
+                source_cid, source_ulevel, source_isadmin,
+                source_cid, source_ulevel, source_isadmin;
+        end if;
+    end if;            
+    
+    
+    -- =============================================================================
+    -- Grab the TARGET MEMBER from the database
+    
+    SELECT cid, userlevel, isadmin
+        INTO target_cid, target_ulevel, target_isadmin
+        FROM Members
+        WHERE mid = target_mid;
+       
+    -- Check for: [Target-Member] doesn't exist -> NO
+    if (target_cid is null) then
+        return query select RETVAL_ERR_TARGET_NOTFOUND, nil, nil, nil, nil, nil, nil;
+        return;
+    end if;
+    
+    -----------------------------------------------------------------------------
+    
+    if ( -- Three main conditions will allow the Source to change the Target:
+    
+        (source_isadmin = 1)                     -- Admins can change anyone.        
+             
+        or (    (source_ulevel = ULEVEL_OWNER)   -- Owners can change anyone
+            and (source_cid = target_cid)        --  in the same Account.
+        )
+      
+        or (    (source_ulevel = ULEVEL_TRUSTED) -- Trusteds can change anyone
+            and (source_cid = target_cid)        --  in the same Account
+            and (target_ulevel <> ULEVEL_OWNER)  --  except the Owner.
+        )
+        
+    ) then
+    
+        return query select RETVAL_SUCCESS,
+                source_cid, source_ulevel, source_isadmin,
+                target_cid, target_ulevel, target_isadmin;
+
+    -----------------------------------------------------------------------------
+    
+    else -- Not allowed    
+        return query select RETVAL_ERR_NOT_ALLOWED, nil, nil, nil, nil, nil, nil;
         
     end if;
-   
-    
-    -- If you got this far then yes, sourceMember can update targetMember
-    
-    return query select RETVAL_SUCCESS,
-            source_cid, source_userlevel, source_isadmin,
-            target_cid, target_userlevel, target_isadmin;
-    return;    
-    
+
 end;
 
 $$ language plpgsql;
