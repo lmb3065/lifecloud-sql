5172c7c94880ea29eb1f7a8dad9b88ac4c283a96
David A. Brown <dabrown7@gmail.com>
2013-11-12 18:42:03 -0800

    log_permissions_error: Replaced specific retvals with NOT_ALLOWED

--- functions/func-log_permissions_error.sql
+++ functions/func-log_permissions_error.sql
@@ -7,6 +7,7 @@
 -- 2013-10-13 dbrown Created
 -- 2013-10-16 dbrown returns void now
 -- 2013-11-06 dbrown Updated result code meanings, function now returns result code
+-- 2013-11-12 dbrown Replaced specific retvals with NOT_ALLOWED
 -- ----------------------------------------------------------------------------------------------
 
 create or replace function log_permissions_error(
@@ -24,9 +25,7 @@ declare
     RETVAL_ERR_ARG_MISSING      constant int := 0;
     RETVAL_ERR_MEMBER_NOTFOUND  constant int := -11;
     RETVAL_ERR_TARGET_NOTFOUND  constant int := -12;
-    RETVAL_ERR_USERLEVEL        constant int := -80;
-    RETVAL_ERR_DIFFACCOUNTS     constant int := -81;
-    RETVAL_ERR_OUTRANKED        constant int := -82;
+    RETVAL_ERR_NOT_ALLOWED      constant int := -88;
    
     msg text;
     
@@ -36,9 +35,8 @@ begin
         when RETVAL_ERR_ARG_MISSING     then msg := 'A required argument was null';
         when RETVAL_ERR_MEMBER_NOTFOUND then msg := 'Source Member does not exist';
         when RETVAL_ERR_TARGET_NOTFOUND then msg := 'Target Member does not exist';
-        when RETVAL_ERR_USERLEVEL       then msg := 'Source Member has insufficient permissions';
-        when RETVAL_ERR_DIFFACCOUNTS    then msg := 'Source and Target are in different Accounts';
-        when RETVAL_ERR_OUTRANKED       then msg := 'Target Member outranks Source Member';
+        when RETVAL_ERR_NOT_ALLOWED     then msg := 'Source Member is not allowed to do that.';
+        else                                 msg := '(Unrecognized permissions error code!)';
     end case;
 
     perform log_event( scid, smid, code, msg, tcid, tmid );
