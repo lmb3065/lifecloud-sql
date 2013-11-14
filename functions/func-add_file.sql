99db53151dbd3f70e7d4fd2d0c83c5b98e5b8112
David A. Brown <dabrown7@gmail.com>
2013-11-13 22:04:05 -0800

    organization, exception handling, more info in EventCodes

--- functions/func-add_file.sql
+++ functions/func-add_file.sql
@@ -23,7 +23,9 @@
 --                    added source level logging
 -- 2013-11-10 dbrown : update to latest revision of eventcodes/retvals
 --                    add filename to success event log
--- 2013-11-12 dbrown : add filename to failure event log too; raise warnings
+-- 2013-11-12 dbrown : add filename to failure event log too, raise notices,
+--                     simplified user-level eventcode logging
+-- 2013-11-13 dbrown : Organization, Exception Handling, More info in events
 -- -----------------------------------------------------------------------------
 
 create or replace function add_file(
@@ -36,16 +38,16 @@ create or replace function add_file(
 ) returns int as $$
 
 declare
-    EC_OK_ADDED_FILE            constant varchar := '1080';
-    EC_OK_OWNER_ADDED_FILE      constant varchar := '1081';
-    EC_OK_ADMIN_ADDED_FILE      constant varchar := '1082';
-    EC_USERERR_ADDING_FILE      constant varchar := '4080';
-    EC_PERMERR_ADDING_FILE      constant varchar := '6080';
-    EC_DEVERR_ADDING_FILE       constant varchar := '9080';
+    EVENT_OK_ADDED_FILE            constant varchar := '1080';
+    EVENT_OK_OWNER_ADDED_FILE      constant varchar := '1081';
+    EVENT_OK_ADMIN_ADDED_FILE      constant varchar := '1082';
+    EVENT_USERERR_ADDING_FILE      constant varchar := '4080';
+    EVENT_AUTHERR_ADDING_FILE      constant varchar := '6080';
+    EVENT_DEVERR_ADDING_FILE       constant varchar := '9080';
 
-    RETVAL_ERR_ARG_MISSING      constant int :=   0;
+    RETVAL_ERR_ARG_INVALID      constant int :=   0;
     RETVAL_ERR_FOLDER_NOTFOUND  constant int := -13;
-    RETVAL_ERR_FILE_EXISTS      constant int := -27;
+    RETVAL_ERR_FILE_EXISTS      constant int := -26;
 
     result          int;
     source_cid      int;
@@ -59,77 +61,80 @@ declare
         
 begin
 
-    -- Make sure source user exists
     
-    if not exists (select mid from members where mid = source_mid)
-        raise notice 'add_file(): Fail - Source MID [%] does not exist', source_mid;
-        perform log_event( null, null, EC_DEVERR_ADDING_FILE,
-                    'Source MID '||source_mid||' does not exist' );
+    -- Check arguments -------------------------------------------------------------
+
+    -- Ensure a name was supplied
+    if (_name is null) or (length(_name) = 0) then
+        perform log_event( null, source_mid, EC_DEVERR_ADDING_FILE, 'Name is required' );
+        return RETVAL_ERR_ARG_INVALID;
+    end if;
+
     
-    -- Get folder's owner
-    
+    -- Ensure destination folder exists (and get its owner)
     select mid into target_mid from folders where uid = parent_folder_uid;
     if (target_mid is null) then
-        raise notice 'add_file(): Fail - Folder [%] does not exist', parent_folder_uid; 
         perform log_event( null, source_mid, EC_DEVERR_ADDING_FILE,
                     'Folder '||parent_folder_uid||' does not exist' );
         return RETVAL_ERR_FOLDER_NOTFOUND; 
     end if;
 
     
-    -- Check relations between user and folder's owner
-
+    -- Ensure user is allowed to modify Folder Owner's Stuff
     select allowed, scid, slevel, sisadmin, tcid 
         into result, source_cid, source_level, source_isadmin, target_cid
         from member_can_update_member(source_mid, target_mid);
     if (result < 1) then 
-        perform log_permissions_error( EC_PERMERR_ADDING_FILE, result,
+        perform log_permissions_error( EVENT_AUTHERR_ADDING_FILE, result,
                   source_cid, source_mid, target_cid, target_mid );
         return result;
     end if;
     
-    -- Validate arguments
-    
-    if (_name is null) or (length(_name) = 0) then
-        raise notice 'add_file(): Fail - Name is required';
-        perform log_event( null, source_mid, EC_DEVERR_ADDING_FILE, 'Name is required' );
-        return RETVAL_ERR_ARG_MISSING;
-    end if;
-    
-    -- Ensure file doesn't already exist in folder 
-    
+    -- Ensure folder doesn't already contain this file    
     select uid into existing_uid from Files
         where folder_uid = parent_folder_uid
         and lower(fdecrypt(x_name)) = lower(_name);
     if (existing_uid is not null) then
-        raise notice 'add_file(): Fail - A file named "%" already exists in folder [%]', _name, parent_folder_uid;
-        perform log_event( source_cid, source_mid, EC_USERERR_ADDING_FILE,
-            'A file named "'||_name||'" already exists in folder "'||parent_folder_uid, target_cid, target_mid);
+        perform log_event( source_cid, source_mid, EC_USERERR_ADDING_FILE, 'File ['
+            ||existing_uid||'] named "'||_name||'" already exists in folder ['
+            ||parent_folder_uid||']', target_cid, target_mid);
         return RETVAL_ERR_FILE_EXISTS;
     end if;
+ 
     
     
-    -- Add file to database
+    -- Add file to database --------------------------------------------------------
     
-    insert into Files ( folder_uid, mid, x_name, x_desc, modified_by )
+    declare
+        errno text;
+        errmsg text;
+        errdetail text;
+    begin        
+        insert into Files ( folder_uid, mid, x_name, x_desc, modified_by )
         values ( parent_folder_uid, target_mid, 
             fencrypt(_name), fencrypt(_desc), source_mid );
+                
+    exception when others then
+        -- Couldn't add File!
+        get stacked diagnostics errno=RETURNED_SQLSTATE, errmsg=MESSAGE_TEXT, errdetail=PG_EXCEPTION_DETAIL;                
+        perform log_event(_cid, null, EVENT_DEVERR_ADDING_FILE, '['||errno||'] '||errmsg||' : '||errdetail);
+        RETURN RETVAL_ERR_EXCEPTION;
+    end;
 
-
-    -- Success
     
-    if (source_isadmin = 1) then
-        eventcode_out := EC_OK_ADMIN_ADDED_FILE;
-    elsif (source_level = 0) and (source_mid <> target_mid) then
-        eventcode_out := EC_OK_OWNER_ADDED_FILE;
-    else
-        eventcode_out := EC_OK_ADDED_FILE;
-    end if;
     
-    perform log_event( source_cid, source_mid, eventcode_out, _name, target_cid, target_mid );
+    -- Success ---------------------------------------------------------------------
     
     select last_value into newfileuid from files_uid_seq;
-    raise notice 'add_file(): Success - File [%] created in Folder [%]', newfileuid, parent_folder_uid;
+
+    if (source_mid = target_mid) then eventcode_out := EC_OK_ADDED_FILE;
+    elsif (source_isadmin = 1)   then eventcode_out := EC_OK_ADMIN_ADDED_FILE;
+    elsif (source_level   = 0)   then eventcode_out := EC_OK_OWNER_ADDED_FILE;
+    else eventcode_out := EC_OK_ADDED_FILE;
+    end if;
+    
+    perform log_event( source_cid, source_mid, eventcode_out, 
+                '['||newfileuid||'] '||_name, target_cid, target_mid );    
     return newfileuid;
     
 end
