d18130c7714af5a869b926fddfa69b09f96f9c43
David A. Brown <dabrown7@gmail.com>
2013-11-15 14:13:08 -0800

    purge_sessions_before: Added purge counts

--- functions/func-purge_sessions_before.sql
+++ functions/func-purge_sessions_before.sql
@@ -4,11 +4,17 @@
 -------------------------------------------------------------------------
 --     cutoff timestamp : time of earliest session to preserve 
 -- ======================================================================
+-- 2013-11-15 dbrown: Purge counts added (needed by update_session)
   
 create or replace function purge_sessions_before( _dt timestamp )
-    returns void as $$
+    returns int as $$
+declare
+    nrows int;
 begin
     delete from sessions where dtlogin < _dt;
+
+    get diagnostics nrows = row_count;
+    return nrows;
 end;
 $$ language plpgsql;
 
