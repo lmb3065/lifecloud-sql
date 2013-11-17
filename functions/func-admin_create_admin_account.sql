6bed740859a10c211d455af87ad396caf68dbca3
David A. Brown <dabrown7@gmail.com>
2013-11-16 16:36:20 -0800

    New admin account is actually an admin.  Demo account gets made.
    - admin_account: fixed cid/mid mixup that was preventing admin
      from actually getting admin rights
    - demo_account: Replaced NULL that was crashing add_member with ''
      (add_member needs to be modified to handle this)

--- functions/func-admin_create_admin_account.sql
+++ functions/func-admin_create_admin_account.sql
@@ -1,7 +1,7 @@
 -- ==========================================================================
 -- function admin_create_admin_account
 -- --------------------------------------------------------------------------
--- Creates the GOD ACCOUNT.  If it detects that one already exists, 
+-- Creates the GOD ACCOUNT.  If it detects that one already exists,
 --      it will delete it using admin_remove_account_cascade().
 -- This function is run automatically by the database installation script.
 --      Returns information via RAISE instead of return value.
@@ -20,21 +20,21 @@ as $$
 declare
     EVENT_DEVERR_ADDING_ACCOUNT  constant char(4) := '9020';
     RETVAL_SUCCESS               constant int     := 1;
-    
+
     n int;
-    
+
     _cid int;
     _mid int;
     newcid int;
     newmid int;
     expiration  timestamp := current_date + interval '5 years';
-    
+
     C_ADMIN_EMAIL constant varchar := 'admin@lifecloud.info';
     C_ADMIN_PWORD constant varchar := 'admin';
     C_ADMIN_FNAME constant varchar := 'Administrator';
     C_ADMIN_LNAME constant varchar := 'User';
     C_ADMIN_MI    constant varchar := 'J';
-    
+
 begin
 
     -- Check for existing admin accounts
@@ -44,23 +44,23 @@ begin
         return 'Administrator account already exists!';
     end if;
 
-    
+
     -- Create Admin Account
     newcid := add_account(
-        C_ADMIN_EMAIL, C_ADMIN_PWORD, C_ADMIN_LNAME, C_ADMIN_FNAME, 
+        C_ADMIN_EMAIL, C_ADMIN_PWORD, C_ADMIN_LNAME, C_ADMIN_FNAME,
         C_ADMIN_MI, expiration, '', '', '', '', '', '', 'US' );
     if (newcid < RETVAL_SUCCESS) then
         return 'Couldn''t create Administrator account! ('||newcid||')';
     end if;
 
-    
     -- Mark new account's owner member as the Administrator
+    select last_value into newmid from members_mid_seq;
     update Members set isadmin = 1 where mid = newmid;
-    
+
     -- Done
-    return 'Administrator account [#'||newmid||'] created.';
-    
-    
+    return 'Administrator account [#'||newcid||'] and login [#'||newmid||'] created.';
+
+
 end;
 $$ language plpgsql;
 
