eeb44082e8f87c5b2217aa43125675b303b0b383
David A. Brown <dabrown7@gmail.com>
2013-12-11 12:58:06 -0800

    Modifying Folder references to use app_uid (#2)
    * admin_create_defaultfolders: removed itemtype
    * get_folders: replaced itemtype with app_uid
    * add_folder: replaced itemtype with app_uid

--- functions/func-admin_create_defaultfolders.sql
+++ functions/func-admin_create_defaultfolders.sql
@@ -9,6 +9,7 @@
 -- 2013-10-15 dbrown: removed vieworder field
 -- 2013-11-10 dbrown: returns void, communicates via RAISE
 -- 2013-11-14 dbrown: Communicates by returning TEXT
+-- 2013-12-11 dbrown: removed itemtype field
 -- -----------------------------------------------------------------------------
 
 create or replace function admin_create_defaultfolders() returns text as $$
@@ -16,18 +17,18 @@ declare
     nrows int;
 begin
     truncate table ref_defaultfolders cascade;
-    insert into ref_defaultfolders(x_name, x_desc, itemtype) values
-        (fencrypt('Activities'),            fencrypt('Activities folder'),      0),
-        (fencrypt('Car Pool'),              fencrypt('Car Pool folder'),        0),
-        (fencrypt('Education'),             fencrypt('Education folder'),       0),
-        (fencrypt('Emergency'),             fencrypt('Emergency folder'),       0),
-        (fencrypt('Financial Summary'),     fencrypt('Financial folder'),       0),
-        (fencrypt('Insurance'),             fencrypt('Insurance folder'),       0),
-        (fencrypt('Medical'),               fencrypt('Medical folder'),         0),
-        (fencrypt('Memory Box'),            fencrypt('Memory Box folder'),      0),
-        (fencrypt('Passwords'),             fencrypt('Passwords folder'),       0),
-        (fencrypt('Personal Info'),         fencrypt('Personal Info folder'),   0),
-        (fencrypt('To Do List'),            fencrypt('To Do List folder'),      0);
+    insert into ref_defaultfolders(x_name, x_desc) values
+        (fencrypt('Activities'),            fencrypt('Activities folder')),
+        (fencrypt('Car Pool'),              fencrypt('Car Pool folder')),
+        (fencrypt('Education'),             fencrypt('Education folder')),
+        (fencrypt('Emergency'),             fencrypt('Emergency folder')),
+        (fencrypt('Financial Summary'),     fencrypt('Financial folder')),
+        (fencrypt('Insurance'),             fencrypt('Insurance folder')),
+        (fencrypt('Medical'),               fencrypt('Medical folder')),
+        (fencrypt('Memory Box'),            fencrypt('Memory Box folder')),
+        (fencrypt('Passwords'),             fencrypt('Passwords folder')),
+        (fencrypt('Personal Info'),         fencrypt('Personal Info folder')),
+        (fencrypt('To Do List'),            fencrypt('To Do List folder'));
 
     select count(*) into nrows from ref_defaultfolders;
     return 'DefaultFolders reference table loaded: '||nrows||' rows.';
--- functions/func-get_folders.sql
+++ functions/func-get_folders.sql
@@ -2,9 +2,10 @@
 -- ================================================================================
 --  get_folders()
 -- --------------------------------------------------------------------------------
---  2013-10-30 dbrown: Completely rewritten to incorporate get_folder and folder_t
---  2013-11-01 dbrown: Coded to new folders.uid but still outputs it as "fid"
---  2013-11-12 dbrown: Raises warning, rets NULL if neither search term provided
+-- 2013-10-30 dbrown: Completely rewritten to incorporate get_folder and folder_t
+-- 2013-11-01 dbrown: Coded to new folders.uid but still outputs it as "fid"
+-- 2013-11-12 dbrown: Raises warning, rets NULL if neither search term provided
+-- 2013-12-11 dbrown: replaced itemtype with app_uid
 -- --------------------------------------------------------------------------------
 
 create or replace function get_folders(
@@ -23,7 +24,7 @@ create or replace function get_folders(
     memberlname text,
     foldername  text,
     description text,
-    itemtype    int,
+    app_uid     int,
     created     timestamp,
     updated     timestamp,
     nrows       int,
@@ -41,23 +42,23 @@ declare
 begin
 
     if (_folder_uid is not null) then -- Search by folderID
-    
-        create temporary table folders_out on commit drop as        
+
+        create temporary table folders_out on commit drop as
             select f.uid as fid, f.mid, m.cid,
                 fdecrypt(m.x_fname) as memberfname,
                 fdecrypt(m.x_lname) as memberlname,
                 fdecrypt(f.x_name) as foldername,
                 fdecrypt(f.x_desc) as description,
-                f.itemtype, f.created, f.updated
+                f.app_uid, f.created, f.updated
             from Folders f join Members m on (f.mid = m.mid)
-            where f.uid = _folder_uid; 
+            where f.uid = _folder_uid;
 
     elsif (_mid is not null) then -- Search by MemberID
-        
-        -- Is this member an account owner?    
+
+        -- Is this member an account owner?
         select m.cid into _cid from Members m where m.mid = _mid;
         select a.owner_mid into _owner_mid from Accounts a where a.cid = _cid;
-    
+
         if (_mid = _owner_mid) then
             -- Yes: retrieve folders from all members under this account
             create temporary table folders_out on commit drop as
@@ -66,10 +67,10 @@ begin
                     fdecrypt(m.x_lname) as memberlname,
                     fdecrypt(f.x_name) as foldername,
                     fdecrypt(f.x_desc) as description,
-                    f.itemtype, f.created, f.updated
+                    f.app_uid, f.created, f.updated
                 from Folders f join Members M on (f.mid = m.mid)
                 where f.cid = _cid;
-                
+
         else -- No: retrieve folders from only this member
             create temporary table folders_out on commit drop as
                 select f.uid as fid, f.mid, m.cid,
@@ -77,17 +78,17 @@ begin
                     fdecrypt(m.x_lname) as memberlname,
                     fdecrypt(f.x_name) as foldername,
                     fdecrypt(f.x_desc) as description,
-                    f.itemtype, f.created, f.updated
+                    f.app_uid, f.created, f.updated
                 from Folders f join Members m on (f.mid = m.mid)
                 where f.mid = _mid;
-                
+
         end if;
     else
         raise warning 'get_folders(): no search criteria supplied';
         return;
     end if;
 
-    
+
     -- Pagination
     select count(*) into _nrows from folders_out;
     if (coalesce(_pagesize, 0) = 0) then -- No paging = 1 page
@@ -98,14 +99,14 @@ begin
         if (_nrows % _pagesize > 0) then _npages := _npages + 1; end if;
     end if;
 
-    
+
     -- Output results
     return query
         select o.*, _nrows, _npages
         from folders_out o
             order by memberlname asc, memberfname asc, foldername asc
             limit _pagesize offset (_page * _pagesize);
-            
+
     return;
 
 end
