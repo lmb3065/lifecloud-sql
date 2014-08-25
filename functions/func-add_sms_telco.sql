96b0a0a297c234de75a48dfb501b7c8336c9b320
David A. Brown <dabrown7@gmail.com>
2014-08-24 18:41:40 -0700

    fixes to add_sms_telco

--- functions/func-add_sms_telco.sql
+++ functions/func-add_sms_telco.sql
@@ -14,20 +14,21 @@ declare
 begin
 
     declare
+    
         errno text;
         errmsg text;
         errdetail text;
 
     begin
 
-        insert into sms_telcos (telco, suffix)
-        values(_telco, _suffix);
+        INSERT INTO sms_telcos (telco, suffix)
+        VALUES(_telco, _suffix);
 
     exception when others then
 
         -- Couldn't add File!
         get stacked diagnostics errno=RETURNED_SQLSTATE, errmsg=MESSAGE_TEXT, errdetail=PG_EXCEPTION_DETAIL;
-        perform log_event(_cid, null, EVENT_DEVERR_ADDING_FILE, '['||errno||'] '||errmsg||' : '||errdetail);
+        perform log_event(_cid, null, null, '['||errno||'] '||errmsg||' : '||errdetail);
         RETURN RETVAL_ERR_EXCEPTION;
 
     end;
