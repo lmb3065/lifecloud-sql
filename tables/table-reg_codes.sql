7f8d072f1ac65c5a4d64367273a487de7c357925
David A. Brown <dabrown7@gmail.com>
2014-12-31 18:28:16 -0800

    Create new table 'reg_codes'

---
+++ tables/table-reg_codes.sql
@@ -0,0 +1,19 @@
+
+-- -----------------------------------------------------------------------------
+--  table reg_codes
+-- -----------------------------------------------------------------------------
+-- 2014-12-30 dbrown: created
+-- -----------------------------------------------------------------------------
+
+create table reg_codes
+(
+    code            text    not null,
+    maximum_uses    int     not null,
+    code_uses       int     not null  default 0,
+    description     text,
+    code_effective  timestamp not null,
+    code_expires    timestamp,
+    account_expires timestamp,
+    account_life    int
+);
+alter table reg_codes owner to pgsql;
