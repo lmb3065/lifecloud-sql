7b66d159ebaba30f7c12fb9bb2b9eeffc56809eb
David A. Brown <dabrown7@gmail.com>
2015-04-24 15:42:44 -0700

    Add Items Extraction

---
+++ crypto/etl-14.sql
@@ -0,0 +1,30 @@
+
+-- -----------------------------------------------------------------------------
+-- 1.4. Decrypt/Extract Items
+-- -----------------------------------------------------------------------------
+
+create or replace function ETL_14() returns integer as $$
+declare
+    _nrows integer;
+
+begin
+
+    CREATE TABLE _ct_Items as SELECT
+        uid,
+        mid,
+        cid,
+        folder_uid,
+        app_uid,
+        itemtype,
+        fdecrypt(x_name)    as name,
+        fdecrypt(x_desc)    as desc,
+        created,
+        updated,
+        modified_by
+    from Items;
+
+    select count(*) into _nrows from _ct_Items;
+    return _nrows;
+
+end;
+$$ language plpgsql;
--- crypto/etl.sql
+++ crypto/etl.sql
@@ -17,8 +17,8 @@ begin
 
     raise notice 'Extracting Members';      perform ETL_11();
     raise notice 'Extracting Folders';      perform ETL_12();
-    -- raise notice 'Extracting Files';       perform ETL_13();
-    -- raise notice 'Extracting Items';       perform ETL_14();
+    raise notice 'Extracting Files';        perform ETL_13();
+    raise notice 'Extracting Items';        perform ETL_14();
     -- raise notice 'Extracting IPNs';        perform ETL_15();
 
     -- raise notice 'Inserting Members';     perform ETL_21();
