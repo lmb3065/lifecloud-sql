a12c45557ba3e121be0b8b379e116a048f6694fb
David A. Brown <dabrown7@gmail.com>
2015-07-08 19:41:38 -0700

    Remove cleartext fields from ETL

--- crypto/etl-1.sql
+++ crypto/etl-1.sql
@@ -1,21 +1,18 @@
 
 
--- -----------------------------------------------------------------------------
--- 1.1. Decrypt/Extract Members
--- -----------------------------------------------------------------------------
 create or replace function etl1() returns integer as $$
 begin
 
-    raise notice 'Extracting Members';
+-- =============================================================================
+-- 1.1. Decrypt/Extract Members
+-- -----------------------------------------------------------------------------
+    raise notice 'Decrypting Members';
     DROP TABLE IF EXISTS _ct_Members;
 
     CREATE TABLE _ct_Members AS SELECT
         mid,
-        cid,
-        h_passwd,
         fdecrypt(x_userid)      as userid,
         fdecrypt(x_email)       as email,
-        h_profilepic,
         fdecrypt(x_fname)       as fname,
         fdecrypt(x_mi)          as mi,
         fdecrypt(x_lname)       as lname,
@@ -26,112 +23,62 @@ begin
         fdecrypt(x_postalcode)  as postalcode,
         fdecrypt(x_country)     as country,
         fdecrypt(x_phone)       as phone,
-        alerttype,
         fdecrypt(x_alertphone)  as alertphone,
-        fdecrypt(x_alertemail)  as alertemail,
-        status,
-        pwstatus,
-        userlevel,
-        tooltips,
-        isadmin,
-        logincount,
-        created,
-        updated
+        fdecrypt(x_alertemail)  as alertemail
     from Members;
 
-
-
--- -----------------------------------------------------------------------------
+-- =============================================================================
 -- 1.2. Decrypt/Extract Folders
 -- -----------------------------------------------------------------------------
-
-    raise notice 'Extracting Folders';
+    raise notice 'Decrypting Folders';
     DROP TABLE IF EXISTS _ct_Folders;
 
     CREATE TABLE _ct_Folders AS SELECT
         uid,
-        mid,
-        cid,
         fdecrypt(x_name)    as name,
-        fdecrypt(x_desc)    as desc,
-        app_uid,
-        created,
-        updated
+        fdecrypt(x_desc)    as desc
     from Folders;
 
-
-
--- -----------------------------------------------------------------------------
+-- =============================================================================
 -- 1.3. Decrypt/Extract Files
 -- -----------------------------------------------------------------------------
-    raise notice 'Extracting Files';
-
+    raise notice 'Decrypting Files';
     DROP TABLE IF EXISTS _ct_Files;
 
     CREATE TABLE _ct_Files AS SELECT
         uid,
-        folder_uid,
-        mid,
-        item_uid,
         fdecrypt(x_name)        as name,
         fdecrypt(x_desc)        as desc,
-        content_type,
-        isprofile,
-        category,
-        fdecrypt(x_form_data)   as form_data,
-        modified_by,
-        created,
-        updated
+        fdecrypt(x_form_data)   as form_data
     from Files;
 
-
-
-
--- -----------------------------------------------------------------------------
+-- =============================================================================
 -- 1.4. Decrypt/Extract Items
 -- -----------------------------------------------------------------------------
-
-    raise notice 'Extracting Itemss';
-
+    raise notice 'Decrypting Items';
     DROP TABLE IF EXISTS _ct_Items;
 
     CREATE TABLE _ct_Items as SELECT
         uid,
-        mid,
-        cid,
-        folder_uid,
-        app_uid,
-        itemtype,
         fdecrypt(x_name)    as name,
-        fdecrypt(x_desc)    as desc,
-        created,
-        updated,
-        modified_by
+        fdecrypt(x_desc)    as desc
     from Items;
 
-
-
-
--- -----------------------------------------------------------------------------
+-- =============================================================================
 -- 1.5. Decrypt/Extract IPN
 -- -----------------------------------------------------------------------------
-
-    raise notice 'Extracting IPNs';
-
+    raise notice 'Decrypting IPNs';
     DROP TABLE IF EXISTS _ct_IPN;
 
     CREATE TABLE _ct_IPN AS SELECT
         UID,
-        IPNReceived,
         fdecrypt(x_business)        as business,
         fdecrypt(x_receiver_email)  as receiver_email,
         fdecrypt(x_item_name)       as item_name,
         fdecrypt(x_item_number)     as item_number,
-        quantity,
         fdecrypt(x_invoice)         as invoice,
         fdecrypt(x_custom)          as custom,
         fdecrypt(x_memo)            as memo,
-        tax,
         fdecrypt(x_payment_status)  as payment_status,
         fdecrypt(x_pending_reason)  as pending_reason,
         fdecrypt(x_reason_code)     as reason_code,
@@ -139,29 +86,6 @@ begin
         fdecrypt(x_txn_id)          as txn_id,
         fdecrypt(x_txn_type)        as txn_type,
         fdecrypt(x_payment_type)    as payment_type,
-        mc_gross,
-        mc_fee,
-        mc_currency,
-        settle_amount,
-        settle_currency,
-        exchange_rate,
-        payment_gross,
-        payment_fee,
-        subscr_date,
-        subscr_effective,
-        period1,
-        period2,
-        period3,
-        amount1,
-        amount2,
-        amount3,
-        mc_amount1,
-        mc_amount2,
-        mc_amount3,
-        recurring,
-        reattempt,
-        retry_at,
-        recur_times,
         fdecrypt(x_username)        as username,
         fdecrypt(x_password)        as password,
         fdecrypt(x_subscr_id)       as subscr_id,
@@ -176,12 +100,7 @@ begin
         fdecrypt(x_address_status)  as address_status,
         fdecrypt(x_payer_email)     as payer_email,
         fdecrypt(x_payer_id)        as payer_id,
-        fdecrypt(x_payer_status)    as payer_status,
-        notify_version,
-        verify_sign,
-        post_status,
-        post_response,
-        HAStatus
+        fdecrypt(x_payer_status)    as payer_status
     from IPN;
 
     return 0;
--- crypto/etl-3.sql
+++ crypto/etl-3.sql
@@ -1,16 +1,6 @@
 
- -- ETL PART 3 : INSERTION
+ -- ETL PART 3 : RE-ENCRYPTION
 
-
-    alter table files       drop constraint files_mid_fkey;
-    alter table folders     drop constraint folders_mid_fkey;
-    alter table items       drop constraint items_mid_fkey;
-    alter table member_apps drop constraint member_apps_mid_fkey;
-    alter table profilepics drop constraint profilepics_mid_fkey;
-    alter table reminders   drop constraint reminders_mid_fkey;
-    alter table sessions    drop constraint sessions_mid_fkey;    
-    alter table files       drop constraint files_folder_uid_fkey;
-    alter table items       drop constraint items_folder_uid_fkey;
 
 create or replace function etl3() returns int as $$
 declare
@@ -24,208 +14,93 @@ declare
 begin
 
     -----------------------------------------------------------------------------
-    raise notice 'Adding Folders';
+    raise notice 'Re-Encrypting Folders';
 
-    truncate table Folders;
     for r in folders_c loop
-        insert into Folders (
-            uid, mid, cid, x_name, x_desc,
-            app_uid, created, updated )
-        values (
-            r.uid, r.mid, r.cid, fencrypt(r.name), fencrypt(r.desc),
-            r.app_uid, r.created, r.updated );
+        update Folders set
+            x_name = fencrypt(r.name),
+            x_desc = fencrypt(r.desc)
+        where Folders.uid = r.uid;
     end loop;
 
     -----------------------------------------------------------------------------
-    raise notice 'Adding Items';
+    raise notice 'Re-Encrypting Items';
 
-    truncate table Items;
     for r in items_c loop
-        insert into Items (
-            uid, mid, cid, 
-            folder_uid, app_uid, itemtype,
-            x_name, x_desc, 
-            created, updated, modified_by )
-        values (
-            r.uid, r.mid, r.cid, 
-            r.folder_uid, r.app_uid, r.itemtype,
-            fencrypt(r.name), fencrypt(r.desc), 
-            r.created, r.updated, r.modified_by );
+        update Items set
+            x_name = fencrypt(r.name),
+            x_desc = fencrypt(r.desc)
+        where Items.uid = r.uid;
     end loop;
 
     -----------------------------------------------------------------------------
-    raise notice 'Adding Files';
+    raise notice 'Re-Encrypting Files';
 
-    truncate table Files;
     for r in files_c loop
-        insert into Files (
-            uid, folder_uid, mid, item_uid,
-            x_name, x_desc,
-            content_type, isprofile, category,
-            x_form_data, modified_by, created, updated )
-        values (
-            r.uid, r.folder_uid, r.mid, r.item_uid,
-            fencrypt(r.name), fencrypt(r.desc),
-            r.content_type, r.isprofile, r.category,
-            fencrypt(r.form_data), r.modified_by, r.created, r.updated );
+        update Files set
+            x_name      = fencrypt(r.name),
+            x_desc      = fencrypt(r.desc),
+            x_form_data = fencrypt(r.form_data)
+        where Files.uid = r.uid;
     end loop;
 
     -----------------------------------------------------------------------------
-    raise notice 'Adding Members';
+    raise notice 'Re-Encrypting Members';
 
-    truncate table Members;
     for r in members_c loop
-        insert into Members (
-            mid, cid, h_passwd, x_userid, x_email,
-            h_profilepic,
-            x_fname, x_mi, x_lname,
-            x_address1, x_address2, x_city,
-            x_state, x_postalcode, x_country,
-            x_phone,
-            alerttype, x_alertphone, x_alertemail,
-            status, pwstatus, userlevel, tooltips, 
-            isadmin, logincount, created, updated )
-        values (
-            r.mid, r.cid, r.h_passwd, fencrypt(r.userid), fencrypt(r.email),
-            r.h_profilepic,
-            fencrypt(r.fname), fencrypt(r.mi), fencrypt(r.lname),
-            fencrypt(r.address1), fencrypt(r.address2), fencrypt(r.city),
-            fencrypt(r.state), fencrypt(r.postalcode), fencrypt(r.country),
-            fencrypt(r.phone),
-            r.alerttype, fencrypt(r.alertphone), fencrypt(r.alertemail),
-            r.status, r.pwstatus, r.userlevel, r.tooltips,
-            r.isadmin, r.logincount, r.created, r.updated );
+        update Members set
+            x_userid   = fencrypt(r.userid),
+            x_email    = fencrypt(r.email),
+            x_fname    = fencrypt(r.fname),
+            x_mi       = fencrypt(r.mi),
+            x_lname    = fencrypt(r.lname),
+            x_address1 = fencrypt(r.address1),
+            x_address2 = fencrypt(r.address2),
+            x_city     = fencrypt(r.city),
+            x_state    = fencrypt(r.state),
+            x_postalcode = fencrypt(r.postalcode),
+            x_country    = fencrypt(r.country),
+            x_phone      = fencrypt(r.phone),
+            x_alertphone = fencrypt(r.alertphone),
+            x_alertemail = fencrypt(r.alertemail)
+        where Members.mid = r.mid;
     end loop;
 
     -----------------------------------------------------------------------------
-    raise notice 'Adding IPNs';
+    raise notice 'Re-Encrypting IPNs';
 
-    truncate table IPN;
     for r in ipn_c loop
-        insert into IPN (
-            UID,
-            IPNReceived,
-            x_business,
-            x_receiver_email,
-            x_item_name,
-            x_item_number,
-            quantity,
-            x_invoice,
-            x_custom,
-            x_memo,
-            tax,
-            x_payment_status,
-            x_pending_reason,
-            x_reason_code,
-            x_payment_date,
-            x_txn_id,
-            x_txn_type,
-            x_payment_type,
-            mc_gross,
-            mc_fee,
-            mc_currency,
-            settle_amount,
-            settle_currency,
-            exchange_rate,
-            payment_gross,
-            payment_fee,
-            subscr_date,
-            subscr_effective,
-            period1,
-            period2,
-            period3,
-            amount1,
-            amount2,
-            amount3,
-            mc_amount1,
-            mc_amount2,
-            mc_amount3,
-            recurring,
-            reattempt,
-            retry_at,
-            recur_times,
-            x_username,
-            x_password,
-            x_subscr_id,
-            x_first_name,
-            x_last_name,
-            x_address_name,
-            x_address_street,
-            x_address_city,
-            x_address_state,
-            x_address_zip,
-            x_address_country,
-            x_address_status,
-            x_payer_email,
-            x_payer_id,
-            x_payer_status,
-            notify_version,
-            verify_sign,
-            post_status,
-            post_response,
-            HAStatus )
-        values (
-            r.UID,
-            r.IPNReceived,
-            fencrypt(r.business),
-            fencrypt(r.receiver_email),
-            fencrypt(r.item_name),
-            fencrypt(r.item_number),
-            r.quantity,
-            fencrypt(r.invoice),
-            fencrypt(r.custom),
-            fencrypt(r.memo),
-            r.tax,
-            fencrypt(r.payment_status),
-            fencrypt(r.pending_reason),
-            fencrypt(r.reason_code),
-            fencrypt(r.payment_date),
-            fencrypt(r.txn_id),
-            fencrypt(r.txn_type),
-            fencrypt(r.payment_type),
-            r.mc_gross,
-            r.mc_fee,
-            r.mc_currency,
-            r.settle_amount,
-            r.settle_currency,
-            r.exchange_rate,
-            r.payment_gross,
-            r.payment_fee,
-            r.subscr_date,
-            r.subscr_effective,
-            r.period1,
-            r.period2,
-            r.period3,
-            r.amount1,
-            r.amount2,
-            r.amount3,
-            r.mc_amount1,
-            r.mc_amount2,
-            r.mc_amount3,
-            r.recurring,
-            r.reattempt,
-            r.retry_at,
-            r.recur_times,
-            fencrypt(r.username),
-            fencrypt(r.password),
-            fencrypt(r.subscr_id),
-            fencrypt(r.first_name),
-            fencrypt(r.last_name),
-            fencrypt(r.address_name),
-            fencrypt(r.address_street),
-            fencrypt(r.address_city),
-            fencrypt(r.address_state),
-            fencrypt(r.address_zip),
-            fencrypt(r.address_country),
-            fencrypt(r.address_status),
-            fencrypt(r.payer_email),
-            fencrypt(r.payer_id),
-            fencrypt(r.payer_status),
-            r.notify_version,
-            r.verify_sign,
-            r.post_status,
-            r.post_response,
-            r.HAStatus );
+        update IPN set
+            x_business        = fencrypt(r.business),
+            x_receiver_email  = fencrypt(r.receiver_email),
+            x_item_name       = fencrypt(r.item_name),
+            x_item_number     = fencrypt(r.item_number),
+            x_invoice         = fencrypt(r.invoice),
+            x_custom          = fencrypt(r.custom),
+            x_memo            = fencrypt(r.memo),
+            x_payment_status  = fencrypt(r.payment_status),
+            x_pending_reason  = fencrypt(r.pending_reason),
+            x_reason_code     = fencrypt(r.reason_code),
+            x_payment_date    = fencrypt(r.payment_date),
+            x_txn_id          = fencrypt(r.txn_id),
+            x_txn_type        = fencrypt(r.txn_type),
+            x_payment_type    = fencrypt(r.payment_type),
+            x_username        = fencrypt(r.username),
+            x_password        = fencrypt(r.password), 
+            x_subscr_id       = fencrypt(r.subscr_id),
+            x_first_name      = fencrypt(r.first_name),
+            x_last_name       = fencrypt(r.last_name),
+            x_address_name    = fencrypt(r.address_name),
+            x_address_street  = fencrypt(r.address_street),
+            x_address_city    = fencrypt(r.address_city),
+            x_address_state   = fencrypt(r.address_state),
+            x_address_zip     = fencrypt(r.address_zip),
+            x_address_country = fencrypt(r.address_country),
+            x_address_status  = fencrypt(r.address_status),
+            x_payer_email     = fencrypt(r.payer_email),
+            x_payer_id        = fencrypt(r.payer_id),
+            x_payer_status    = fencrypt(r.payer_status)
+        where IPN.uid = r.uid;
     end loop;
 
     -----------------------------------------------------------------------------
@@ -234,11 +109,3 @@ begin
 end;
 $$ language plpgsql;
 
-
-/* 
-    ETL_21 Encrypt/Insert Members
-    ETL_22 Encrypt/Insert Folders
-    ETL_23 Encrypt/Insert Files
-    ETL_24 Encrypt/Insert Items
-    ETL_25 Encrypt/Insert IPN
-*/
