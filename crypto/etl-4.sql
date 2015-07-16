c047e02a3a1b924179acbcb8bc514acd47bb9268
David A. Brown <dabrown7@gmail.com>
2015-07-16 14:02:47 -0700

    Add ETL Cleanup

---
+++ crypto/etl-4.sql
@@ -0,0 +1,10 @@
+
+drop table _ct_Folders;
+drop table _ct_Items;
+drop table _ct_Files;
+drop table _ct_Members;
+drop table _ct_IPN;
+drop table _ct_Events;
+drop table _ct_DefaultFolders;
+drop table _ct_Sessions;
+
