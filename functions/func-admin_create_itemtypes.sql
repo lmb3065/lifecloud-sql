805d287c50e92b293a171afd148213ae300a96d8
David A. Brown <dabrown7@gmail.com>
2015-08-28 10:06:26 -0700

    Update ref_itemtypes with new contents

--- functions/func-admin_create_itemtypes.sql
+++ functions/func-admin_create_itemtypes.sql
@@ -34,7 +34,22 @@ begin
         ('items'),                  -- 17
         ('reserved'),               -- 18
         ('reserved'),               -- 19
-        ('vehicle');                -- 20
+        ('vehicle'),                -- 20
+        ('emergency'),              -- 21
+        ('memory'),                 -- 22
+        ('item location list'),     -- 23
+        ('insurance policy'),       -- 24
+        ('banking document'),       -- 25
+        ('activity'),               -- 26
+        ('funeral plan'),           -- 27
+        ('major purchase'),         -- 28
+        ('to-do list'),             -- 29
+        ('vacation'),               -- 30
+        ('tax item'),               -- 31
+        ('babysitter'),             -- 32
+        ('caregiver'),              -- 33
+        ('carpool'),                -- 34
+        ('subscription');           -- 35
     select count(*) from ref_itemtypes into nrows;
     return 'ItemTypes reference table loaded: '||nrows||' rows.';
 
