9c0c341051fca26f8f667ea28fd358430066cc46
David A. Brown <dabrown7@gmail.com>
2015-09-04 14:58:26 -0700

    Refine function get_login_history
    Add _my_cid arg and permissions checking
    If _my_cid is admin and target is 0, get everyone

--- functions/func-get_login_history.sql
+++ functions/func-get_login_history.sql
@@ -1,6 +1,7 @@
 
 create or replace function get_login_history(
 
+    _my_mid         int,
     _target_mid     int,
     _from           timestamp   default now() - interval '7 days',
     _to             timestamp   default now(),
@@ -23,22 +24,54 @@ create or replace function get_login_history(
 
 declare
 
+    RETVAL_SUCCESS              constant int := 1;
+    EVENT_AUTHERR_GETTING_EVENT constant varchar := '6096';
     _nrows int;
     _npages int;
+    _result int;
+    _sisadmin int;
 
 begin
 
     if (_from is null) then _from := now() - interval '7 days'; end if;
     if (_to   is null) then _to   := now();                     end if;
 
-    create temporary table events_out on commit drop as
-        select e.eid, e.dt, e.code, re.description,
-            cast(fdecrypt(e.x_data) as varchar) as event,
-            e.cid, e.mid, 0 as nrows, 0 as npages
-        from events e left outer join ref_eventcodes re on (e.code = re.code)
-        where (e.dt between _from and _to)
-          and (e.mid = _target_mid)
-          and (e.code in ('1000','1001'));
+    if (_target_mid = 0) then
+        select isadmin into _sisadmin from members m where m.mid = _my_mid;
+        if (_sisadmin <> 1) then
+            perform log_permissions_error( EVENT_AUTHERR_GETTING_EVENT, _result, 
+                null, _my_mid, null, _target_mid );
+            return;
+        end if;
+
+        create temporary table events_out on commit drop as
+            select e.eid, e.dt, e.code, re.description,
+                cast(fdecrypt(e.x_data) as varchar) as event,
+                e.cid, e.mid, 0 as nrows, 0 as npages
+            from events e left outer join ref_eventcodes re on (e.code = re.code)
+            where (e.dt between _from and _to)
+              and (e.code in ('1000','1001'));
+
+    else
+
+        select allowed, sisadmin into _result, _sisadmin
+            from member_can_view_member(_my_mid, _target_mid);
+        if (result < 1) then
+            perform log_permissions_error( EVENT_AUTHERR_GETTING_EVENT, _result, 
+                null, _my_mid, null, _target_mid );
+            return;
+        end if;
+
+        create temporary table events_out on commit drop as
+            select e.eid, e.dt, e.code, re.description,
+                cast(fdecrypt(e.x_data) as varchar) as event,
+                e.cid, e.mid, 0 as nrows, 0 as npages
+            from events e left outer join ref_eventcodes re on (e.code = re.code)
+            where (e.dt between _from and _to)
+              and (e.mid = _target_mid)
+              and (e.code in ('1000','1001'));
+
+    end if;
 
     -- Calculate pages from the temp table
     select count(*) into _nrows from events_out;    
