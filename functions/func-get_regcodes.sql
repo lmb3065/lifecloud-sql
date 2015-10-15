a1d079d118ffe224d88e2bee90779d4b91c52b62
David A. Brown <dabrown7@gmail.com>
2015-10-15 10:09:26 -0700

    Add new fields to get_regcodes()

--- functions/func-get_regcodes.sql
+++ functions/func-get_regcodes.sql
@@ -5,7 +5,7 @@
 -- 2015-01-17 dbrown : Fix paging
 -- 2015-03-23 dbrown : Add column 'paypal_button_id'
 -- 2015-06-27 dbrown : Add paypal columns periodN/amountN
---  drop function get_regcodes(int, int, int);
+-- 2015-10-15 dbrown : Add 2nd paypal button columns
 
 create or replace function get_regcodes
 (
@@ -24,13 +24,20 @@ returns table
     account_expires  timestamp,
     account_life     int,
     discount         int,
-    paypal_button_id varchar(16),
-    period1           varchar(4),
-    period2           varchar(4),
-    period3           varchar(4),
-    amount1           varchar(10),
-    amount2           varchar(10),
-    amount3           varchar(10),
+    paypal_button_id_1  varchar(16),
+    period1_1           varchar(4),
+    period2_1           varchar(4),
+    period3_1           varchar(4),
+    amount1_1           varchar(10),
+    amount2_1           varchar(10),
+    amount3_1           varchar(10),
+    paypal_button_id_2  varchar(16),
+    period1_2           varchar(4),
+    period2_2           varchar(4),
+    period3_2           varchar(4),
+    amount1_2           varchar(10),
+    amount2_2           varchar(10),
+    amount3_2           varchar(10),
     nrows            int,
     npages           int
 ) as $$
@@ -70,9 +77,13 @@ begin
     return query
         select rc.code, rc.maximum_uses, rc.code_uses, rc.description,
             rc.code_effective, rc.code_expires, rc.account_expires, 
-            rc.account_life, rc.discount, rc.paypal_button_id, 
-            rc.period1, rc.period2, rc.period3,
-            rc.amount1, rc.amount2, rc.amount3,
+            rc.account_life, rc.discount, 
+            rc.paypal_button_id_1, 
+            rc.period1_1, rc.period2_1, rc.period3_1,
+            rc.amount1_1, rc.amount2_1, rc.amount3_1,
+            rc.paypal_button_id_2, 
+            rc.period1_2, rc.period2_2, rc.period3_2,
+            rc.amount1_2, rc.amount2_2, rc.amount3_2,
             _nrows, _npages
         from reg_codes rc
         order by rc.code asc
