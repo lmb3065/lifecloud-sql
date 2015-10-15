4d7d42566149151f9d08079d686f0b5179c9c423
David A. Brown <dabrown7@gmail.com>
2015-10-14 17:51:09 -0700

    Alter reg_codes table structure
    Add new _2 instance of PayPal fields

--- tables/table-reg_codes.sql
+++ tables/table-reg_codes.sql
@@ -7,6 +7,7 @@
 -- 2015-01-15 dbrown: add column discount (int)
 -- 2015-03-23 dbrown: add column paypal_button_id
 -- 2015-06-27 dbrown: add columns periodN, amountN
+-- 2015-10-14 dbrown: Add 2nd group of Paypal fields
 -- -----------------------------------------------------------------------------
 
 create table reg_codes
@@ -20,24 +21,39 @@ create table reg_codes
     account_expires timestamp,
     account_life    int,
     discount        int,
-    paypal_button_id varchar(16),
-    period1         char(1),
-    period2         char(1),
-    period3         char(1),
-    amount1         varchar(10),
-    amount2         varchar(10),
-    amount3         varchar(10)
+    paypal_button_id_1 varchar(16),
+    period1_1       char(1),
+    period2_1       char(1),
+    period3_1       char(1),
+    amount1_1       varchar(10),
+    amount2_1       varchar(10),
+    amount3_1       varchar(10)
+    paypal_button_id_2 varchar(16),
+    period1_2       char(1),
+    period2_2       char(1),
+    period3_2       char(1),
+    amount1_2       varchar(10),
+    amount2_2       varchar(10),
+    amount3_2       varchar(10)
 );
+
 alter table reg_codes owner to pgsql;
 
 /* Latest change:
 
-    alter table reg_codes
-        add period1 varchar(4),
-        add period2 varchar(4),
-        add period3 varchar(4),
-        add amount1 varchar(10),
-        add amount2 varchar(10),
-        add amount3 varchar(10); 
+    alter table reg_codes rename paypal_button_id   to paypal_button_id_1;
+    alter table reg_codes rename period1            to period1_1;
+    alter table reg_codes rename period2            to period2_1;
+    alter table reg_codes rename period3            to period3_1;
+    alter table reg_codes rename amount1            to amount1_1;
+    alter table reg_codes rename amount2            to amount2_1;
+    alter table reg_codes rename amount3            to amount3_1;
+    alter table reg_codes add                          paypal_button_id_2   varchar(16);
+    alter table reg_codes add                          period1_2            char(1);
+    alter table reg_codes add                          period2_2            char(1);
+    alter table reg_codes add                          period3_2            char(1);
+    alter table reg_codes add                          amount1_2            varchar(10);
+    alter table reg_codes add                          amount2_2            varchar(10);
+    alter table reg_codes add                          amount3_2            varchar(10);
 
 */
